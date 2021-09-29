8bded4b27ec238deac84a6ce3cb33607e2c86389
Kamyar mazloom <mazloom1994@gmail.com>
2021-09-29 12:57:16 +0330

    created main.js using webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import{headerCreation} from '/header.js'
-import{homeCreation} from '/home.js'
+import{headerCreation} from './header.js'
+import{homeCreation} from './home.js'
 
 window.onload = (event) => {
     headerCreation()
